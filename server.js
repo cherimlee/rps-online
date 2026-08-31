bcbe79c4da020e8af9fc35fef32b39eef2657100
SolgitCherim <lcr@cudo.co.kr>
2026-08-31 11:31:59 +0900

    배포 환경의 포트를 쓰도록 수정

--- server.js
+++ server.js
@@ -81,4 +81,5 @@ wss.on('connection', (ws) => {
   });
 });
 
-server.listen(3000, () => console.log('http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
