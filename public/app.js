ba1fe29b27b8b35f50c27c1fac9896ff4dae376e
SolgitCherim <lcr@cudo.co.kr>
2026-08-31 14:28:40 +0900

    https 배포 환경에서 wss로 연결하도록 수정

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const $ = (id) => document.getElementById(id);
-const ws = new WebSocket(`ws://${location.host}`);
+const proto = location.protocol === 'https:' ? 'wss' : 'ws';
+const ws = new WebSocket(`${proto}://${location.host}`);
 
 $('join').onclick = () => {
   ws.send(JSON.stringify({
